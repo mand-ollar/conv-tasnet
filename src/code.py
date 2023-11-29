e7380db0c695a417f01a717998541f11f3548d5f
Anthony Oh <minsec9912@icloud.com>
2023-11-29 11:38:52 +0900

    separation-part commit

--- src/code.py
+++ src/code.py
@@ -63,18 +63,18 @@ class Norm(nn.Module):
         L = x.size(2)
         org_x = x.clone()
         
-        mean = torch.zeros(batch_size, L)
-        variance = torch.zeros(batch_size, L)
+        mean = torch.zeros(batch_size, 1, L)
+        variance = torch.zeros(batch_size, 1, L)
         
-        gamma = nn.Parameter(torch.ones(batch_size, N))
-        beta = nn.Parameter(torch.zeros(batch_size, N))
+        gamma = nn.Parameter(torch.ones(batch_size, N, 1))
+        beta = nn.Parameter(torch.zeros(batch_size, N, 1))
         
         for k in range(L):
-            mean[:, k] = 1 / (N * (k+1)) * torch.sum(org_x[:, :, :k+1])
-            variance[:, k] = 1 / (N * (k+1)) * torch.sum((org_x[:, :, :k+1] - mean[:, k]) ** 2)
+            mean[:, :, k] = 1 / (N * (k+1)) * torch.sum(org_x[:, :, :k+1], dim=(1,2)).unsqueeze(dim=1)
+            variance[:, :, k] = 1 / (N * (k+1)) * torch.sum((org_x[:, :, :k+1] - mean[:, :, k:k+1]) ** 2, dim=(1,2)).unsqueeze(dim=1)
             
-            x[:, :, k] = gamma * (x[:, :, k] - mean[:, k]) / torch.sqrt(variance[:, k] + self.epsilon) + beta
-        
+            x[:, :, k:k+1] = gamma * (org_x[:, :, k:k+1] - mean[:, :, k:k+1]) / torch.sqrt(variance[:, :, k:k+1] + self.epsilon) + beta
+            
         return x
 
 class OneDConvBlock(nn.Module):
@@ -105,8 +105,8 @@ class OneDConvBlock(nn.Module):
                                 )
         
         self.d_conv = nn.ModuleList(
-            [nn.Conv1d(in_channels=H,
-                       out_channels=H,
+            [nn.Conv1d(in_channels=1,
+                       out_channels=1,
                        kernel_size=P,
                        stride=1,
                        dilation=2 ** (dilation - 1),
@@ -137,9 +137,9 @@ class OneDConvBlock(nn.Module):
         x = self.conv1x1(x)
         x = self.prelue1(x)
         x = self.normalize(x)
-        
+
         for i in range(x.size(1)):
-            x[:,i] = self.d_conv[i](x[:,i])
+            x[:, i:i+1] = self.d_conv[i](x[:,i:i+1])
         x = self.prelue2(x)
         x = self.normalize(x)
         
@@ -147,3 +147,99 @@ class OneDConvBlock(nn.Module):
         skip = self.conv1x1_skip(x)
         
         return out, skip
+
+class OneDStack(nn.Module):
+    def __init__(self,
+                 B: int,
+                 H: int,
+                 P: int,
+                 Sc: int,
+                 dilate_X : int,
+                 ):
+        super().__init__()
+        
+        self.Sc = Sc
+        
+        self.dilate_X = dilate_X
+        
+        self.convstack = nn.ModuleList(
+            [OneDConvBlock(B=B,
+                           H=H,
+                           P=P,
+                           Sc=Sc,
+                           dilation=i+1
+                           ) for i in range(dilate_X)]
+        )
+        
+    def forward(self,
+                x: torch.Tensor,
+                ) -> torch.Tensor:
+        
+        sum = torch.zeros(x.size(0), self.Sc, x.size(2))
+        
+        for k in range(self.dilate_X):
+            x, skip = self.convstack[k](x)
+            sum += skip
+        
+        return x, sum
+
+class Separation(nn.Module):
+    def __init__(self,
+                 B: int,
+                 H: int,
+                 P: int,
+                 Sc: int,
+                 L: int,
+                 dilate_X: int,
+                 ):
+        super().__init__()
+        
+        self.dilate_X = dilate_X
+        self.Sc =Sc
+        
+        self.layer_norm = nn.LayerNorm([B, L])
+        
+        self.conv1x1_in = nn.Conv1d(in_channels=B,
+                                    out_channels=B,
+                                    kernel_size=1,
+                                    stride=1,
+                                    )
+        
+        self.one_d_stack = OneDStack(B=B,
+                                     H=H,
+                                     P=P,
+                                     Sc=Sc,
+                                     dilate_X=dilate_X,
+                                     )
+        
+        self.prelu = nn.PReLU(num_parameters=1,
+                              init=0.25,
+                              )
+        
+        self.conv1x1_out = nn.Conv1d(in_channels=Sc,
+                                     out_channels=B,
+                                     kernel_size=1,
+                                     stride=1,
+                                     )
+        
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self,
+                x: torch.Tensor,
+                ) -> torch.Tensor:
+        
+        org_x = x.clone()
+        sum = torch.zeros(x.size(0), self.Sc, x.size(2))
+        
+        x = self.layer_norm(x)
+        x = self.conv1x1_in(x)
+        for _ in range(self.dilate_X):
+            x, skip = self.one_d_stack(x)
+            sum += skip
+        x = self.prelu(skip)
+        x = self.conv1x1_out(x)
+        x = self.sigmoid(x)
+        
+        x = org_x * x
+        
+        return x
